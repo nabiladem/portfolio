bc534518cc4a78354a806590517464f17f79b2cb
Nabil Adem <nabiladam7@gmail.com>
2026-01-03 23:32:40 -0600

    Create basic site

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-50">
+      <header className="text-center space-y-4">
+        <h1 className="text-4xl font-bold text-gray-800">Nabil Adem</h1>
+        <p className="text-lg text-gray-500">Software Developer</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
