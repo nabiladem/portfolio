184d3967a24c259a164dbbb7a7da72be82d57f66
Nabil Adem <nabiladam7@gmail.com>
2026-01-07 19:40:02 -0600

    Complete bio

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         whileHover="hovered"
-        className="text-center space-y-12 p-18 w-full max-w-6xl liquid-glass relative overflow-hidden"
+        className="text-center space-y-6 p-12 w-full max-w-6xl liquid-glass relative overflow-hidden"
       >
         <motion.img
           src="/images/headshot.jpg"
@@ -38,14 +38,19 @@ function App() {
           Nabil Adem
         </motion.h1>
 
-        <h2 className="text-2xl font-semibold text-slate-300 mt-2">Software Developer</h2>
+        <h2 className="text-2xl font-semibold text-slate-300">Software Developer</h2>
 
-        <p className="text-lg text-slate-500 mt-2">
-          <span className="block">👋🏾🌍</span>
-          I am Nabil Adem, a software developer with a passion for creating innovative solutions.
+        <p className="text-xl text-slate-500">
+          <span className="block text-3xl">👋🏾🌍</span>
+          I am passionate about creating innovative solutions to real-world challenges.
+          I enjoy building user-focused applications, learning new technologies, collaborating with others,
+          and strengthening my skills through hands-on projects.
+          I am motivated by the satisfaction of overcoming challenges and turning ideas into effective solutions.
+          Through my curiosity, I am a reliable contributor who delivers meaningful impact to any team, whether
+          by implementing scalable systems or improving user experiences at startups or large organizations.
         </p>
 
-        <div className="flex items-center justify-center gap-5 mt-6 text-sm text-slate-400 liquid-glass p-4">
+        <div className="flex items-center justify-center gap-5 mt-6 text-sm text-slate-400 liquid-glass p-4 px-10 w-fit mx-auto">
           <span>📍 Based in Phoenix, AZ & Nashville, TN</span>
           <span className="text-slate-600">|</span>
           <span className="text-slate-200 font-medium">Open to relocation</span>
